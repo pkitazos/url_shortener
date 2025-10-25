b5961a3d06f2835a13bc7b36e1d9d9b56cb80521
Petros <93052465+pkitazos@users.noreply.github.com>
2025-10-25 17:53:05 +0200

    wip: trying to resolve race condition

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::HashMap,
     error::Error,
+    hash::{DefaultHasher, Hash, Hasher},
     sync::{Arc, Mutex},
 };
 
@@ -16,15 +17,15 @@ use sqlx::{FromRow, Pool, Sqlite, SqlitePool};
 #[derive(Debug, Clone)]
 struct AppCtx {
     pool: Pool<Sqlite>,
-    stl_cache: Arc<Mutex<HashMap<String, String>>>, // coule probably do with better names
-    lts_cache: Arc<Mutex<HashMap<String, String>>>,
+    short_to_long_cache: Arc<Mutex<HashMap<String, String>>>, // coule probably do with better names
+    long_to_short_cache: Arc<Mutex<HashMap<String, String>>>,
 }
 
 impl AppCtx {
     fn new(pool: Pool<Sqlite>) -> AppCtx {
         AppCtx {
-            stl_cache: Arc::new(Mutex::new(HashMap::new())),
-            lts_cache: Arc::new(Mutex::new(HashMap::new())),
+            short_to_long_cache: Arc::new(Mutex::new(HashMap::new())),
+            long_to_short_cache: Arc::new(Mutex::new(HashMap::new())),
             pool,
         }
     }
@@ -63,9 +64,9 @@ async fn root() -> impl IntoResponse {
 }
 
 fn cool_shortener(long_url: &String) -> String {
-    // for now we just trim the string
-    // the actual shortening mechanic is not super important
-    long_url[1..7].to_owned()
+    let mut s = DefaultHasher::new();
+    long_url.hash(&mut s);
+    format!("{:x}", s.finish())
 }
 
 /// C -> S : shorten(long_url) ... S -> C : success(short_code)
@@ -73,58 +74,48 @@ async fn shorten(
     State(ctx): State<AppCtx>,
     Query(params): Query<HashMap<String, String>>,
 ) -> impl IntoResponse {
-    if let Some(long_url) = params.get("q") {
-        println!("/shorten POST <-- {}", long_url);
+    if let None = params.get("q") {
+        return (StatusCode::BAD_REQUEST, "URL was not provided".to_owned());
+    }
 
-        {
-            // acquire lock on lts
-            let lts_cache = ctx.lts_cache.lock().unwrap();
-            if let Some(short_code) = lts_cache.get(long_url) {
-                println!("\tfound in cache");
-                // already in cache, means already in db, can just return
-                return (StatusCode::OK, short_code.to_owned());
-            }
-            // release lock on lts
-        }
+    let long_url = params.get("q").unwrap();
+    println!("/shorten POST <-- {}", long_url);
 
+    let short_code = cool_shortener(&long_url);
+
+    let mut long_to_short_cache = ctx.long_to_short_cache.lock().unwrap();
+    if let Some(short_code) = long_to_short_cache.get(long_url) {
+        println!("\tfound in cache");
+        // already in cache, means already in db, can just return
+        (StatusCode::OK, short_code.to_owned())
+    } else {
+        // not in cache, so add it
         println!("\tcache miss - new entry");
-        let short_code = cool_shortener(&long_url);
         println!("\tshortened to: {}", short_code);
+        long_to_short_cache.insert(long_url.clone(), short_code.clone());
+        println!("\tstoring in stl cache");
 
         let url = URL {
             long_url: long_url.clone(),
-            short_code: short_code.to_owned(),
+            short_code: short_code.clone(),
         };
 
-        if let Ok(_) = store_entry(&url, &ctx.pool).await {
-            println!("\tsaved to db");
-            // already checked that it's not in the cache
-
-            {
-                // acquire lock on lts
-                let mut lts_cache = ctx.lts_cache.lock().unwrap();
-                lts_cache.insert(long_url.clone(), short_code.clone());
-                println!("\tstoring in lts cache");
-                // release lock on lts
-            }
-            {
-                // acquire lock on stl
-                let mut stl_cache = ctx.stl_cache.lock().unwrap();
-                stl_cache.insert(short_code.clone(), long_url.clone());
-                println!("\tstoring in stl cache");
-                // release lock on stl
+        match store_entry(url, &ctx.pool).await {
+            Ok(_) => {
+                // already added it to stl cache
+                println!("\tsaved to db");
+                (StatusCode::OK, short_code)
             }
 
-            return (StatusCode::OK, short_code);
-        } else {
-            return (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Something went wrong on our end".to_owned(),
-            );
+            Err(e) => {
+                eprintln!("Failed to store entry: {}", e);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Something went wrong on our end".to_owned(),
+                )
+            }
         }
     }
-
-    (StatusCode::BAD_REQUEST, "URL was not provided".to_owned())
 }
 
 /// C -> S : redirect(short_code) ...  S -> C : {
@@ -133,31 +124,44 @@ async fn shorten(
 /// }
 async fn redirect(State(ctx): State<AppCtx>, Path(short_code): Path<String>) -> impl IntoResponse {
     println!("/redirect GET <-- {}", short_code);
-    {
-        // acquire lock on stl
-        let stl_cache = ctx.stl_cache.lock().unwrap();
-        if let Some(long_url) = stl_cache.get(&short_code) {
-            println!("\tfound in cache");
-            return (StatusCode::OK, long_url.to_owned());
-        }
-        // release lock on stl
+
+    // acquire lock on stl
+    let mut short_to_long_cache = ctx.short_to_long_cache.lock().unwrap();
+    if let Some(long_url) = short_to_long_cache.get(&short_code) {
+        println!("\tfound in cache");
+        return (StatusCode::OK, long_url.to_owned());
     }
+    // release lock on stl
+
     println!("\tcache miss - looking in db");
 
-    if let Ok(Some(url)) = lookup_entry(&short_code, &ctx.pool).await {
-        println!("\tfound in db");
-        return (StatusCode::OK, url.long_url.to_owned());
-    }
+    match lookup_entry(&short_code, &ctx.pool).await {
+        Ok(Some(url)) => {
+            println!("\tfound in db");
 
-    println!("\tnot in db");
-    (
-        StatusCode::NOT_FOUND,
-        "Short code not recognised".to_owned(),
-    )
+            short_to_long_cache.insert(short_code.clone(), url.long_url.clone());
+            println!("\tstoring in stl cache");
+            (StatusCode::OK, url.long_url.to_owned())
+        }
+        Ok(None) => {
+            println!("\tnot in db");
+            (
+                StatusCode::NOT_FOUND,
+                "Short code not recognised".to_owned(),
+            )
+        }
+        Err(e) => {
+            eprintln!("Failed to lookup entry: {}", e);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Something went wrong on our end".to_owned(),
+            )
+        }
+    }
 }
 
 /// S -> D : store(URL) . D -> S : ok() . D -> S : ok() . end,
-async fn store_entry(url: &URL, pool: &sqlx::SqlitePool) -> Result<(), sqlx::Error> {
+async fn store_entry(url: URL, pool: &sqlx::SqlitePool) -> Result<(), sqlx::Error> {
     let long_url = &url.long_url;
     let short_code = &url.short_code;
 
